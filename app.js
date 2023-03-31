8b736612806caaccc714db394d224f8247862fb2
xAkashax <kbilan1992@gmail.com>
2023-03-31 23:00:09 +0200

    add js

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 'use strict';
+const navbar = document.querySelector('.navbar-scroll');
+const links = document.querySelectorAll('.nav-link');
+const scrollupBtn = document.querySelector('.btn-back-to-top');
 
-const navColor = document.querySelector('.navbar-scroll');
-const linksColor = document.querySelectorAll('.nav-link');
+window.onload = function () {
+  window.scrollTo(0, 0);
+};
 
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 0) {
-    navColor.classList.add('scrolled');
-    linksColor.forEach(link => {
-      link.style.color = '#f6f6f6';
-    });
+    navbar.classList.add('scrolled');
   } else {
-    navColor.classList.remove('scrolled');
-    linksColor.forEach(link => {
-      link.style.color = '';
-    });
+    navbar.classList.remove('scrolled');
   }
 });
-
-const links = document.querySelectorAll('.nav-link');
 
 links.forEach(link => {
   link.addEventListener('click', function () {
     links.forEach(link => {
       link.classList.remove('active');
     });
-
     link.classList.add('active');
   });
 });
+
+// Scroll up
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollupBtn.style.display = 'block';
+  } else {
+    scrollupBtn.style.display = 'none';
+  }
+}
+
+scrollupBtn.addEventListener('click', backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
